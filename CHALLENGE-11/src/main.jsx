b04826e26a42eb5923f15ff1f20daa944bd29082
Andressann <98990837+Andressann@users.noreply.github.com>
2023-10-13 20:43:25 -0500

    Add files via upload

---
+++ CHALLENGE-11/src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import FirstApp_Challenge11 from './FirstApp_Challenge11'
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <FirstApp_Challenge11 title = 'Fist App Challenge 11' />
+  </React.StrictMode>,
+)
